8627c442eca232ea2b6ec1276f92fc40ddebd819
Yash Gupta <yashbg7@gmail.com>
2021-06-17 04:33:36 +0530

    Add children_dict

--- code.py
+++ code.py
@@ -17,7 +17,7 @@ def parse_mempool_csv():
         return([MempoolTransaction(*line.strip().split(',')) for line in file.readlines()])
 
 def analyse_mempool(mempool):
-    """Analyse the mempool and print the stats."""
+    """Analyse the mempool and print its stats."""
     num_tx = len(mempool) # TOtal no. of transactions
     print('Total no. of transactions:', num_tx)
 
@@ -35,13 +35,36 @@ def print_mempool(mempool):
     for tx in mempool:
         print(tx.txid, tx.fee, tx.weight, tx.parents)
 
+def create_children_dict(mempool):
+    """Creates and returns a dictionary which contains the list of children of the transaction for every transaction in the mempool."""
+    children_dict = {}
+    for tx in mempool:
+        children_dict[tx.txid] = []
+    for tx in mempool:
+        for parent in tx.parents:
+            children_dict[parent].append(tx.txid)
+    return children_dict
+
+def analyse_children_dict(children_dict):
+    """Analyse the children_dict and print its stats."""
+    num_with_children = 0 # No. of transactions having atleast 1 child transaction
+    max_num_children = 0 # Maximum no. of child transactions of any transaction
+    for children in children_dict.values():
+        if len(children) > 0:
+            num_with_children += 1
+        max_num_children = max(max_num_children, len(children))
+    print("No. of transactions having atleast 1 child transaction:", num_with_children)
+    print("Maximum no. of child transactions of any transaction:", max_num_children)
+
 def save_block_stats(block, total_fee, total_weight):
+    """Save the stats of the block in stats.txt."""
     with open('stats.txt', 'w') as file:
         file.write(f"Total fee: {total_fee} satoshis\n")
         file.write(f"Total weight: {total_weight}\n")
         file.write(f"Size of block: {len(block)} transactions\n")
 
 def check_tx(block, tx, curr_weight):
+    """Check if a transaction can be added to the block."""
     if curr_weight + tx.weight > MAX_WEIGHT:
         return False
     for parent in tx.parents:
@@ -67,7 +90,7 @@ def create_block(sorted_mempool):
     return block, curr_fee, curr_weight
 
 def save_block_txids(block):
-    """Save the transaction identifiers of the block in block.txt"""
+    """Save the transaction identifiers of the block in block.txt."""
     with open('block.txt', 'w') as file:
         for txid in block:
             file.write(txid)
@@ -76,6 +99,10 @@ def save_block_txids(block):
 mempool = parse_mempool_csv()
 
 analyse_mempool(mempool)
+print()
+
+children_dict = create_children_dict(mempool)
+analyse_children_dict(children_dict)
 print()
 
 sorted_mempool = sorted(mempool, key=lambda tx: tx.fee / tx.weight, reverse=True)
