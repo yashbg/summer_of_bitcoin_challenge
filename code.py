2fa6ea4d67bd399af1eab839a002d189cd594097
Yash Gupta <yashbg7@gmail.com>
2021-06-18 03:34:49 +0530

    Add print_block_stats() and rearrange functions

--- code.py
+++ code.py
@@ -6,7 +6,7 @@ class MempoolTransaction:
         self.txid = txid # the transaction identifier
         self.fee = int(fee) # the transaction fee
         self.weight = int(weight) # the transaction weight
-        
+
         # a list of the txids of the transaction’s unconfirmed parent transactions
         if parents == '':
             self.parents = []
@@ -79,39 +79,6 @@ def create_is_added_dict(mempool):
         is_added_dict[tx.txid] = False # block is empty initially
     return is_added_dict
 
-def save_block_stats(block):
-    """Save the stats of the block in stats.txt."""
-    with open('stats.txt', 'w') as file:
-        file.write(f"Total fee: {block.fee} satoshis\n")
-        file.write(f"Total weight: {block.weight}\n")
-        file.write(f"Size of block: {len(block.txids)} transactions\n")
-
-def check_tx(tx, curr_weight):
-    """Check if a transaction can be added to the block."""
-    # cannot be added if the weight limit exceeds or if it is already added or if all of its parents have not been added yet
-    if curr_weight + tx.weight > MAX_WEIGHT or is_added_dict[tx.txid] or not valid_dict[tx.txid]:
-        return False
-    return True
-
-def update_children_validity(tx):
-    """Update valid_dict for children of tx."""
-    for child in children_dict[tx.txid]:
-        # child has become valid only if all of its parents have already been added to the block
-        for parent in child.parents:
-            if not is_added_dict[parent]:
-                break
-        else:
-            valid_dict[child.txid] = True
-
-def add_tx(block, tx):
-    """Add a transaction to the block and update the total weight and total fee corresponding to the block."""
-    block.weight += tx.weight
-    block.fee += tx.fee
-    block.txids.append(tx.txid)
-
-    is_added_dict[tx.txid] = True
-    update_children_validity(tx) # check if the children of tx have become valid now
-
 def create_block(sorted_mempool):
     """Create and return a block from the mempool maximizing the fee to the miner and print its stats and save them."""
     block = Block(txids=[], fee=0, weight=0)
@@ -125,11 +92,47 @@ def create_block(sorted_mempool):
                 if check_tx(tx_prev, block.weight):
                     add_tx(block, tx_prev)
     
+    print_block_stats(block)
     save_block_stats(block)
+    return block
+
+def check_tx(tx, curr_weight):
+    """Check if a transaction can be added to the block."""
+    # cannot be added if the weight limit exceeds or if it is already added or if all of its parents have not been added yet
+    if curr_weight + tx.weight > MAX_WEIGHT or is_added_dict[tx.txid] or not valid_dict[tx.txid]:
+        return False
+    return True
+
+def add_tx(block, tx):
+    """Add a transaction to the block and update the total weight and total fee corresponding to the block."""
+    block.weight += tx.weight
+    block.fee += tx.fee
+    block.txids.append(tx.txid)
+
+    is_added_dict[tx.txid] = True
+    update_children_validity(tx) # check if the children of tx have become valid now
+
+def update_children_validity(tx):
+    """Update valid_dict for children of tx."""
+    for child in children_dict[tx.txid]:
+        # child has become valid only if all of its parents have already been added to the block
+        for parent in child.parents:
+            if not is_added_dict[parent]:
+                break
+        else:
+            valid_dict[child.txid] = True
+
+def print_block_stats(block):
     print(f"Total fee: {block.fee} satoshis")
     print(f"Total weight: {block.weight}")
     print(f"Size of block: {len(block.txids)} transactions")
-    return block
+
+def save_block_stats(block):
+    """Save the stats of the block in stats.txt."""
+    with open('stats.txt', 'w') as file:
+        file.write(f"Total fee: {block.fee} satoshis\n")
+        file.write(f"Total weight: {block.weight}\n")
+        file.write(f"Size of block: {len(block.txids)} transactions\n")
 
 def save_block_txids():
     """Save the transaction identifiers of the block in block.txt."""
